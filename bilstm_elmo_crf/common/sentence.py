22317ec86d5ff9c7444490c7f9b9ad2bb405fd9f
TeamMatSciE <77156193+TeamMatSciE@users.noreply.github.com>
2021-01-08 19:05:18 +0530

    Add files via upload

---
+++ bilstm_elmo_crf/common/sentence.py
@@ -0,0 +1,24 @@
+# 
+# @author: Allan
+#
+
+from typing import List
+
+class Sentence:
+    """
+    The class for the input sentence
+    """
+
+    def __init__(self, words: List[str], pos_tags:List[str] = None,heading : str = "None"):
+        """
+
+        :param words:
+        :param pos_tags: By default, it is not required to have the pos tags, in case you need it/
+        """
+        self.words = words
+        self.pos_tags = pos_tags
+        self.heading = heading
+    def __len__(self):
+        return len(self.words)
+
+
